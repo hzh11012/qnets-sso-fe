fa7f2105da804bd9f32cca0afb2ce6f85c2c2cd7
hzh11012 <917944345@qq.com>
2025-04-17 03:07:52 +0800

    fix: eslint错误

--- src/components/ui/input-otp.tsx
+++ src/components/ui/input-otp.tsx
@@ -42,8 +42,7 @@ function InputOTPSlot({
     index: number;
 }) {
     const inputOTPContext = React.useContext(OTPInputContext);
-    const { char, hasFakeCaret, isActive } =
-        inputOTPContext?.slots[index] ?? {};
+    const { char, isActive } = inputOTPContext?.slots[index] ?? {};
 
     return (
         <div
